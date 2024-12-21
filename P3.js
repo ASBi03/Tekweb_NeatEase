3523a27e021720be4076fcc21a9d1145644dfdf5
ASBi03 <2300016092@webmail.uad.ac.id>
2024-12-21 08:06:30 +0700

    hapus

--- P3.js
+++
@@ -1,9 +0,0 @@
-function gantiFont() {
-    var elemen = document.getElementsByClassName('contoh');
-    for (var i = 0; i < elemen.length; i++) {
-        elemen[i].style.fontFamily = 'Courier New, Courier, monospace';
-    }
-}
-function gantiBackground() {
-    document.body.style.backgroundColor = 'lightblue';
-}
